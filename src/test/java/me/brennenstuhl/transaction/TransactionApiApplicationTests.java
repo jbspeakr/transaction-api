1b34722604f6d3298be557d77c5fbe1abfff1f39
Jan Brennenstuhl <jan.brennenstuhl@immobilienscout24.de>
2016-05-02 17:37:56 +0200

    initial commit - SpringBoot setup

---
+++ src/test/java/me/brennenstuhl/transaction/TransactionApiApplicationTests.java
@@ -0,0 +1,16 @@
+package me.brennenstuhl.transaction;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = TransactionApiApplication.class)
+public class TransactionApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
