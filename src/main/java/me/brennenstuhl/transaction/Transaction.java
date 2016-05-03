4ffbfb06d27c7217c973175749add64b15230df2
Jan Brennenstuhl <jan.brennenstuhl@immobilienscout24.de>
2016-05-03 12:08:37 +0200

    make json property name explicit and mark transactionid as required.

--- src/main/java/me/brennenstuhl/transaction/Transaction.java
+++ src/main/java/me/brennenstuhl/transaction/Transaction.java
@@ -3,22 +3,19 @@ package me.brennenstuhl.transaction;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.keyvalue.annotation.KeySpace;
-
-import java.time.LocalDateTime;
 
 @KeySpace("transaction")
 @Data
 @RequiredArgsConstructor
 public class Transaction {
-  private @Id @JsonIgnore Long transactionId;
+  private @NonNull @Id @JsonIgnore Long transactionId;
+  private @NonNull @JsonProperty("amount") Double amount;
+  private @NonNull @JsonProperty("type") String type;
   private @JsonProperty("parent_id") Long parentId;
-  private @LastModifiedDate @JsonIgnore LocalDateTime lastModifiedDate;
-  private final Double amount;
-  private final String type;
 
   Transaction() {
     this.amount = null;
